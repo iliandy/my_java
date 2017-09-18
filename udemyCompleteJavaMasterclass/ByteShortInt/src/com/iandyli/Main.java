511fc9c0037e756f098a82b342f432e9ed74a59f
iliandy <iandyli@gmail.com>
2017-09-18 00:08:38 -0500

    completed ByteShortInt assignment questions 1-4

--- udemyCompleteJavaMasterclass/ByteShortInt/src/com/iandyli/Main.java
+++ udemyCompleteJavaMasterclass/ByteShortInt/src/com/iandyli/Main.java
@@ -1,24 +1,18 @@
 package com.iandyli;
 
 public class Main {
-
     public static void main(String[] args) {
-        // int has width of 32
-        int myMinVal = -2_147_483_648;
-        int myMaxVal = 2_147_483_647;
-        int myTotal = myMinVal/2;
-        System.out.println("myTotal = " + myTotal);
+//        1. Create a byte variable and set it to any valid byte number.
+//        2. Create a short variable and set it to any valid short number.
+//        3. Create a int variable and set it to any valid int number.
+//        4. Create a long variable, make it equal to 50000 + 10 times sum of byte, plus short plus int
 
-        // byte has width of 8
-        byte myByteVal = -128;
-        byte myNewByte = (byte) (myByteVal/2);
-        System.out.println("myNewByte = " + myNewByte);
+        byte myByte = 10;
+        short myShort = 30;
+        int myInt = 100;
+        long myLong = 50000L + 10L * (myByte + myShort + myInt);
+        short shortTotal = (short) (1000 + 10 * (myByte + myShort + myInt));
 
-        // short has width of 16
-        short myShortVal = 32767;
-        short myNewShort = (short) (myShortVal/2);
-
-        // long has width of 64
-        long myLongVal = -9_223_372_036_854_775_807L;
+        System.out.println("myLong = " + myLong);
     }
 }

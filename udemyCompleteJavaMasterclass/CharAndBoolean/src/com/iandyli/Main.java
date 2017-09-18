2e2c50cfeea0a57706d4e87a83fcfcbf315e82c9
iliandy <iandyli@gmail.com>
2017-09-18 16:51:14 -0500

    completed CharAndBoolean assignment

---
+++ udemyCompleteJavaMasterclass/CharAndBoolean/src/com/iandyli/Main.java
@@ -0,0 +1,14 @@
+package com.iandyli;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean isHappy = true;
+
+//        1. Find code for registered symbol on same line as copyright symbol.
+//        2. Create a variable of type char and assign it unicode value for that symbol.
+//        3. Display symbol output.
+        char regSym = '\u00AE';
+        System.out.println("regSym: " + regSym);
+    }
+}

b5280a16ae763c67ed0e0c692cb1cff42eaf5615
iliandy <iandyli@gmail.com>
2017-09-18 17:04:46 -0500

    completed Strings assignment

---
+++ udemyCompleteJavaMasterclass/Strings/src/com/iandyli/Main.java
@@ -0,0 +1,27 @@
+package com.iandyli;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	    byte
+//      short
+//      int
+//      long
+//      float
+//      double
+//      char
+//      boolean
+
+        String myStr = "This is a string.";
+        System.out.println(myStr);
+
+        String numStr = "10";
+        int myInt = 33;
+        numStr += myInt;
+        System.out.println(numStr);
+
+        double myDou = 33.33d;
+        numStr += myDou;
+        System.out.println(numStr);
+    }
+}

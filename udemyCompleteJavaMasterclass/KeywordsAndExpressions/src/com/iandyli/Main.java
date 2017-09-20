eeb5c2fedb5305f6f11ef204bcead92653687d4f
iliandy <iandyli@gmail.com>
2017-09-19 23:46:46 -0500

    completed KeywordsAndExpressions assignment

---
+++ udemyCompleteJavaMasterclass/KeywordsAndExpressions/src/com/iandyli/Main.java
@@ -0,0 +1,18 @@
+package com.iandyli;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	      keywords will be highlighted in blue by IntelliJ IDEA
+        int int2 = 3;
+
+//        In following code, what parts of code are expressions
+        int score = 100;
+        if (score > 99) {
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+
+//        solution: score = 100, score > 99, "You got the high score!", score = 0
+    }
+}

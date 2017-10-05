fd38703ca9b1ba821a212128f73bd2509585ed04
iliandy <iandyli@gmail.com>
2017-10-05 15:08:06 -0500

    started OOPMasterProject assignment

---
+++ udemyCompleteJavaMasterclass/Section8_CompositionEncapsulationPolymorphism/OOPMasterProject/src/com/iandyli/Main.java
@@ -0,0 +1,22 @@
+package com.iandyli;
+
+// The purpose of the application is to help a fictitious company called Bills Burgers to manage
+// their process of selling hamburgers.
+// Our application will help Bill to select types of burgers, some of the additional items (additions) to
+// be added to the burgers and pricing.
+// We want to create a base hamburger, but also two other types of hamburgers that are popular ones in
+// Bills store.
+// The basic hamburger should have the following items.
+// bread roll type, meat and up to 4 additional additions (things like lettuce, tomato, carrot, etc) that
+// the customer can select to be added to the burger. Each one of these items gets charged an additional
+// price so you need some way to track how many items got added and to calculate the price (for the base
+// burger and all the additions).
+// This burger has a base price and the additions are all seperately priced.
+// Create a Hamburger class to deal with all the above.
+// The constructor should only include the roll type, meat and price.
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}

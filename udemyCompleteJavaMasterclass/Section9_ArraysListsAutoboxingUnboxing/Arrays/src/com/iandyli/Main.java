0ca47e8457fc55812a336e1628a8998fbc1b63fb
iliandy <iandyli@gmail.com>
2017-10-08 17:17:45 -0500

    completed Arrays assignment, but need to reverse int array order

--- udemyCompleteJavaMasterclass/Section9_ArraysListsAutoboxingUnboxing/Arrays/src/com/iandyli/Main.java
+++ udemyCompleteJavaMasterclass/Section9_ArraysListsAutoboxingUnboxing/Arrays/src/com/iandyli/Main.java
@@ -1,5 +1,9 @@
 package com.iandyli;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 // Create a program using arrays that sorts a list of integers in descending order.
 // Descending order is highest value to lowest.
 // In other words if the array had the values in it 106, 26, 81, 5, 15 your program should
@@ -13,7 +17,36 @@ package com.iandyli;
 // array and sort them and return the new sorted array.
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int[] getIntegers(int num) {
+        System.out.println(String.format("Please enter %d integers.\r", num));
+
+        int[] ints = new int[num];
+
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = scanner.nextInt();
+        }
+
+        return ints;
+    }
+
+    public static void printArray(int[] numArr) {
+        System.out.println(Arrays.toString(numArr));
+    }
+
+    public static int[] sortIntegers(int[] numArr) {
+        int[] sortedArr = Arrays.copyOf(numArr, numArr.length);
+        Arrays.sort(sortedArr);
+        return sortedArr;
+    }
+
+
     public static void main(String[] args) {
-	// write your code here
+	    int[] myInts = getIntegers(5);
+
+	    printArray(myInts);
+        printArray(sortIntegers(myInts));
+
     }
 }
